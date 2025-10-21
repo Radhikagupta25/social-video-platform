a087df74e23b88f051e2cd1bd5c1298929d9b17a
radhikagupta25 <radhikaaaguptaaa25@gmail.com>
2025-10-21 16:36:54 +0530

    fix bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,8 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
-import {ApiError} from "../utils/ApiError.js"
+import ApiError from "../utils/ApiError.js"
 import { User} from "../models/user.model.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
-import { ApiResponse } from "../utils/ApiResponse.js";
+import ApiResponse from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken"
 
 const generateAccessAndRefereshTokens = async (userId) => {
@@ -73,8 +73,6 @@ const registerUser = asyncHandler(async (req, res) => {
 
 const loginUser = asyncHandler(async (req, res) => {
     const { email, username, password } = req.body
-    console.log(email);
-
     if (!username && !email) {
         throw new ApiError(400, "username or email is required")
     }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,6 +3,8 @@ import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { loginUser } from "../controllers/user.controller.js";
+import { logoutUser } from "../controllers/user.controller.js";
 
 const router = Router()
 
